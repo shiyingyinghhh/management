c4650bb10d6fdf633fa027617646776c11461cc0
shiyingying <2330578615qq.com>
2024-10-25 12:16:34 +0800

    已经实现菜单和右侧布局

--- src/router/routes/modules/index.js
+++ src/router/routes/modules/index.js
@@ -1,82 +1,84 @@
 import { DEFAULT_LAYOUT } from '@/router/constants'
 import { PROJECT_LAYOUT } from '../base';
 
-export default {
-  path: '/',
-  name: 'root',
- 
+const projectDetailRoute = {
+  path: '/projects/project/:projectNumber',
+  name: 'projectDetail',
+  component: PROJECT_LAYOUT,
   meta: {
-    order: 0,
+    requiresAuth: true,
+    icon: 'icon-dashboard', 
+    hideInMenu: true,
   },
   children: [
     {
-      path: 'home',
-      name: 'home',
-      component: DEFAULT_LAYOUT,
-      component: () => import('@/views/home/index.vue'),
-      meta: {
-        title: '首页',
-        icon: 'icon-home',
-        order: 0,
-      }
-    },
-    {
-      path: '/projects',
-      name: 'projectManagement',
-      component: DEFAULT_LAYOUT,
-      component: () => import('@/views/projects/index.vue'),
-      meta: {
-        title: '项目管理',
-      }
-    },
-    {
-      path: '/projects/project/:projectNumber',
-      name: 'projectDetail',
-      component: PROJECT_LAYOUT,
+      path: '',
+      name: 'ProjectOverview',
+      component: () => import('@/views/projects/ProjectDetail.vue'),
       meta: {
         requiresAuth: true,
-        icon: 'icon-dashboard',
-        hideInMenu: true,
+        roles: ['*'],
       },
-      children: [
-        {
-          path: '',
-          name: 'ProjectOverview',
-          component: () => import('@/views/projects/ProjectDetail.vue'),
-          meta: {
-            requiresAuth: true,
-            roles: ['*'],
-          },
-        },
-        {
-          path: 'template',
-          name: 'templateManagement',
-          component: () => import('@/views/template/index.vue'),
-          meta: {
-            title: '证书模板',
-            order: 2,
-          },
-        },
-        {
-          path: 'material',
-          name: 'materialManagement',
-          component: () => import('@/views/material/index.vue'),
-          meta: {
-            title: '模板素材',
-            order: 3,
-          },
-        },
-        {
-          path: 'certificate',
-          name: 'certificateManagement',
-          component: () => import('@/views/certificate/index.vue'),
-          meta: {
-            title: '证书管理',
-            icon: 'icon-file',
-            order: 4,
-          },
-        },
-      ],
     },
-  ]
-}
+    {
+      path: 'template',
+      name: 'templateManagement',
+      component: () => import('@/views/template/index.vue'),
+      meta: {
+        title: '证书模板',
+        order: 2,
+      },
+    },
+    {
+      path: 'material',
+      name: 'materialManagement',
+      component: () => import('@/views/material/index.vue'),
+      meta: {
+        title: '模板素材',
+        order: 3,
+      },
+    },
+    {
+      path: 'certificate',
+      name: 'certificateManagement',
+      component: () => import('@/views/certificate/index.vue'),
+      meta: {
+        title: '证书管理',
+        icon: 'icon-file',
+        order: 4,
+      },
+    },
+  ],
+};
+
+export default [
+  {
+    path: '/',
+    name: 'root',
+    component: DEFAULT_LAYOUT,
+    meta: {
+      order: 0,
+    },
+    children: [
+      {
+        path: 'home',
+        name: 'home',
+        component: () => import('@/views/home/index.vue'),
+        meta: {
+          title: '首页',
+          icon: 'icon-home',
+          order: 0,
+        }
+      },
+      {
+        path: '/projects',
+        name: 'projectManagement',
+        component: () => import('@/views/projects/index.vue'),
+        meta: {
+          title: '项目管理',
+        }
+      },
+    ]
+  },
+  projectDetailRoute
+]
